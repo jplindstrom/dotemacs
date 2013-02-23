f09b654a74d7e707a776f19ada8c1bd3b3d78ce1
Johan Lindstrom <jpl@nospam>
2013-02-23 20:45:58 +0000

    Replace magit with packaged version

--- config/tool-magit.el
+++ config/tool-magit.el
@@ -1,23 +1,27 @@
 
 ;; Git
-(setq load-path (cons (concat emacs-home-directory "elisp/magit") load-path))
-(setq load-path (cons "/home/j.lindstrom/dev/personal/magit" load-path))
-;; change magit diff colors
-(eval-after-load 'magit
-  '(progn
-     (set-face-foreground 'magit-diff-add "blue3")
-     (set-face-foreground 'magit-diff-del "red3")
-     (set-face-background 'magit-item-highlight "DarkSeaGreen1")
-     ))
+
+;; (setq load-path (cons (concat emacs-home-directory "elisp/magit") load-path))
+;; (setq load-path (cons "/home/j.lindstrom/dev/personal/magit" load-path))
 (require 'magit)
 ;; (require 'magit-svn)
 
+
+;; change magit diff colors
+(set-face-foreground 'magit-diff-add "blue3")
+(set-face-foreground 'magit-diff-del "red3")
+(set-face-background 'magit-item-highlight "DarkSeaGreen1")
+
+
+;; Avoid re-flymaking all open buffers when checking out a new branch
 (require 'flymake)
 (defadvice magit-revert-buffers (around magit-disable-flymake-during-revert)
   (setq flymake-start-syntax-check-on-find-file nil)
   ad-do-it
   (setq flymake-start-syntax-check-on-find-file t))
 (ad-activate 'magit-revert-buffers)
+
+
 
 (defun magit-commit-message-ticket-number ()
   (interactive)
@@ -40,19 +44,21 @@
     (insert (concat (magit-get-current-branch) ": "))))
 
 
-(defun magit-get-tracking-name (remote branch)
-  "Given a REMOTE and a BRANCH name, ask the user for a local
-tracking brach name suggesting a sensible default."
-  (when (yes-or-no-p
-         (format "Create local tracking branch for %s? " branch))
-    (let* ((default-name (concat (replace-regexp-in-string "[/]" "-" branch)))
-           (chosen-name (read-string (format "Call local branch (%s): " default-name)
-                                     nil
-                                     nil
-                                     default-name)))
-      (when (magit-ref-exists-p (concat "refs/heads/" chosen-name))
-        (error "'%s' already exists." chosen-name))
-      chosen-name)))
+;; Avoid using / in local tracking branch names
+;; TODO: set the magit-default-tracking-name-function var instead!
+;; (defun magit-get-tracking-name (remote branch)
+;;   "Given a REMOTE and a BRANCH name, ask the user for a local
+;; tracking brach name suggesting a sensible default."
+;;   (when (yes-or-no-p
+;;          (format "Create local tracking branch for %s? " branch))
+;;     (let* ((default-name (concat (replace-regexp-in-string "[/]" "-" branch)))
+;;            (chosen-name (read-string (format "Call local branch (%s): " default-name)
+;;                                      nil
+;;                                      nil
+;;                                      default-name)))
+;;       (when (magit-ref-exists-p (concat "refs/heads/" chosen-name))
+;;         (error "'%s' already exists." chosen-name))
+;;       chosen-name)))
 
 
 ;; (require 'vc-git)
