c63a92f3b374eb0a23447eb35e0fb04cb5cc9122
Johan Lindstrom <johan.lindstrom@net-a-porter.com>
2013-10-07 11:24:48 -0400

    Disable autopair in graphviz-dot-mode

--- config/mode-autopair.el
+++ config/mode-autopair.el
@@ -1,6 +1,7 @@
 
 (require 'autopair)
+
+(add-hook 'graphviz-dot-mode-hook #'(lambda () (setq autopair-dont-activate t)))
+
 (autopair-global-mode) ;; to enable in all buffers
 (setq autopair-autowrap t)
-
-
