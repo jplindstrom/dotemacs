8e98fdaa34f424aa81d10be081268a0e7577cbff
Johan Lindstrom <johan.lindstrom@net-a-porter.com>
2013-11-08 11:44:36 -0500

    sparkline

---
+++ elpa/sparkline-20130803.1442/sparkline-autoloads.el
@@ -0,0 +1,18 @@
+;;; sparkline-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("sparkline-pkg.el" "sparkline.el") (21114
+;;;;;;  15281 431222 0))
+
+;;;***
+
+(provide 'sparkline-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sparkline-autoloads.el ends here
---
+++ elpa/sparkline-20130803.1442/sparkline-pkg.el
@@ -0,0 +1,1 @@
+(define-package "sparkline" "20130803.1442" "Make sparkline images from a list of numbers [github]" (quote nil))
