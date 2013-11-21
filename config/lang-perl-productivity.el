6c2cd767845ce01b4a213dfab3968dc440314887
Johan Lindstrom <johan.lindstrom@net-a-porter.com>
2013-11-21 12:45:26 -0500

    lp/recenter-top-of-defun

--- config/lang-perl-productivity.el
+++ config/lang-perl-productivity.el
@@ -85,9 +85,92 @@ If there is a region, wrap the block around the region.
      )
    )
 
+(defun lp/point-of-previous-defun ()
+  "Return point of the previous defun, or nil if there isn't one"
+  (save-excursion
+    (if (beginning-of-defun)
+        (point)
+      nil)))
+
+(defun lp/point-of-pod-line-with (end pod-text)
+  "Return point of POD line which contains POD-TEXT between point
+and END (which should be earlier than point) or nil if there is
+none."
+  (save-excursion
+    (let ((case-fold-search nil))
+      (if (search-backward-regexp
+           ;; Matches POD directive, and also POD-TEXT as a word, e.g.
+           ;; =head2 foo
+           ;; =item $self->foo($bar)
+           (format "^=\\w+\\ .*?\\b%s\\b" (regexp-quote pod-text))
+           end
+           t
+           )
+          (progn
+            (beginning-of-line)
+            (point))
+        nil
+        ))))
+
+(defun lp/sub-name ()
+  "Return the name of the sub at point, or nil if there isn't one"
+  (save-excursion
+    (if (looking-at "\\ *sub\\ +\\(\\w+\\)")
+        (match-string 1)
+      nil)
+    ))
+
+(defun lp/point-of-sub-pod ()
+  "Return point of the POD heading/whatever of sub at point.
+
+It is assumed point is on a line with
+
+  sub foo
+
+and there's an earlier line
+
+  =SOME_POD_DIRECTIVE optional something, something foo
+
+in between the sub foo and the sub preceeding it (or the
+beginning of the buffer if there isn't one).
+"
+  (let ((sub-name (lp/sub-name))
+        (end (or (lp/point-of-previous-defun) (point-min)))
+        )
+    (if (and sub-name end)
+        (lp/point-of-pod-line-with end sub-name)
+      nil
+      ))
+  )
+
+(defun lp/beginning-of-sub-pod ()
+  "Move point to the POD line of the sub at point, or leave it be
+  if there isn't one."
+  (let ((pod-point (lp/point-of-sub-pod)))
+    (when pod-point (goto-char pod-point)))
+  )
+
+(defun lp/recenter-top-of-defun ()
+  "Move to the beginning of the current defun and put that line
+at the top of the window."
+  (interactive)
+
+  (when (not (lp/sub-name))                     ;; If not yet at the sub {}
+    (when (save-excursion (beginning-of-defun)) ;; If we can move to sub {}
+      (beginning-of-defun)))                    ;; Move to the sub {}
+  (when (lp/sub-name)
+    (lp/beginning-of-sub-pod)
+
+    ;; Nicked from window.el -- recenter-top-bottom
+    (let ((this-scroll-margin
+           (min (max 0 scroll-margin)
+                (truncate (/ (window-body-height) 4.0))))
+          )
+
+      (recenter this-scroll-margin))
+    )
+  )
 
 
 ;; (jpl/add-to-load-path "lib/lang-refactor-perl")
 ;; (require 'lang-refactor-perl)
-
-
