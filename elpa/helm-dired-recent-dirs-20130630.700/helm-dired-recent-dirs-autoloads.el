e102be9f75308c01dad604fe9c226651220c9779
Johan Lindstrom <johan.lindstrom@net-a-porter.com>
2013-12-18 05:38:13 -0500

    Upgrade helm, helm-*, recompile all .elc files

---
+++ elpa/helm-dired-recent-dirs-20130630.700/helm-dired-recent-dirs-autoloads.el
@@ -0,0 +1,29 @@
+;;; helm-dired-recent-dirs-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (helm-dired-recent-dirs-view) "helm-dired-recent-dirs"
+;;;;;;  "helm-dired-recent-dirs.el" (21169 30408 0 0))
+;;; Generated autoloads from helm-dired-recent-dirs.el
+
+(autoload 'helm-dired-recent-dirs-view "helm-dired-recent-dirs" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-dired-recent-dirs-pkg.el") (21169
+;;;;;;  30408 300842 0))
+
+;;;***
+
+(provide 'helm-dired-recent-dirs-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-dired-recent-dirs-autoloads.el ends here
---
+++ elpa/helm-dired-recent-dirs-20130630.700/helm-dired-recent-dirs-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-dired-recent-dirs" "20130630.700" "Show recent dirs with helm.el support." (quote ((helm "1.0"))))
