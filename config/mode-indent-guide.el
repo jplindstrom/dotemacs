6d2ee1809a58d894a1fb773bb8dcd4457028ef1f
Johan Lindstrom <jpl@nospam>
2014-10-10 22:34:29 +0100

    indent-guide config for hiding column 0

--- config/mode-indent-guide.el
+++ config/mode-indent-guide.el
@@ -1,6 +1,9 @@
 
+(require 'indent-guide)
 (indent-guide-global-mode)
 
 (set-face-foreground 'indent-guide-face "lightgray")
 (setq indent-guide-char "|")
 
+(setq indent-guide-show-beyond-column 0)
+(setq indent-guide-recursive nil)
