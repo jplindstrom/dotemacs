7ef1cca2344ac8c03fe12a65f4be14ed5d45aa7c
Johan Lindstrom <johan.lindstrom@broadbean.com>
2014-11-06 11:48:01 +0000

    Don't put an error message in PATH if .bash_profile is missing

--- config/emacs-set-path-from-env.el
+++ config/emacs-set-path-from-env.el
@@ -2,7 +2,7 @@
 
 (setenv "PATH"
         (shell-command-to-string
-         "bash -c '. $HOME/.bash_profile; echo -n $PATH'"))
+         "bash -c 'if [ -r $HOME/.bash_profile ]; then . $HOME/.bash_profile; fi; echo -n $PATH'"))
 (setenv "PATH" (concat "/usr/local/git/bin" ":" (getenv "PATH")))
 
 (dolist (dir (split-string (getenv "PATH") ":" t))
