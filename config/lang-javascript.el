134e3a466cb47c24e98c700b0f5617cebbc478e5
Johan Lindstrom <johan.lindstrom@broadbean.com>
2014-12-02 15:47:22 +0000

    js3-mode config

---
+++ config/lang-javascript.el
@@ -0,0 +1,19 @@
+
+(add-hook
+ 'js3-mode-hook
+ (lambda ()
+   (setq js3-auto-indent-p t
+         js3-curly-indent-offset 0
+         js3-enter-indents-newline nil
+         js3-expr-indent-offset 0
+         js3-paren-indent-offset 0
+         js3-square-indent-offset 0
+         js3-indent-dots t
+         js3-lazy-dots t
+         ;; js3-lazy-commas t
+         ;; js3-lazy-operators t
+         )))
+
+
+
+
