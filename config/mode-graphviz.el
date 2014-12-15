b58217807613f49e3f54950c2ad5a50289716978
Johan Lindstrom <johan.lindstrom@broadbean.com>
2014-12-15 15:45:54 +0000

    Graphviz config vars

--- config/mode-graphviz.el
+++ config/mode-graphviz.el
@@ -2,6 +2,10 @@
 ;; GraphViz
 ;; (load-file "~/elisp/graphviz-dot-mode.el")
 
+
+(setq graphviz-dot-auto-indent-on-newline nil)
+(setq graphviz-dot-auto-indent-on-semi nil)
+
 (fset 'jpl-graphviz-preview
    [?\M-x ?c ?o ?m ?p ?i ?l ?e return return ?\C-c ?p])
 
