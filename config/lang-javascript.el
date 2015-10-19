54841a8c4302bcdfda27bb6874778b3a80322643
Johan Lindstrom <johan.lindstrom@broadbean.com>
2015-10-19 09:32:54 +0000

    Replace js3 with js2

--- config/lang-javascript.el
+++ config/lang-javascript.el
@@ -1,22 +1,2 @@
 
-(add-hook
- 'js3-mode-hook
- (lambda ()
-   (setq js3-auto-indent-p t
-         js3-consistent-level-indent-inner-bracket t
-         js3-curly-indent-offset 0
-         js3-enter-indents-newline nil
-         js3-expr-indent-offset 0
-         js3-paren-indent-offset 0
-         js3-square-indent-offset 0
-         js3-indent-dots t
-         js3-lazy-dots t
-         ;; js3-lazy-commas t
-         ;; js3-lazy-operators t
-
-         evil-shift-width 2
-         )))
-
-
-
-
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
