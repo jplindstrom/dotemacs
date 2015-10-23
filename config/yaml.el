f6c201cc98173a51ae988adb48427a0e4286c57e
Johan Lindstrom <johan.lindstrom@broadbean.com>
2015-10-23 10:17:43 +0000

    Add .yaml to auto-mode-alist

--- config/yaml.el
+++ config/yaml.el
@@ -1,7 +1,8 @@
 
 ;; YAML
 (require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yml$"  . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
 
 
