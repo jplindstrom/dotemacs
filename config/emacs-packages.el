29f1cc33b60c8881522fe3ef59805711261e0c75
Johan Lindstrom <johan.lindstrom@broadbean.com>
2018-05-14 12:21:38 +0000

    melpa url

--- config/emacs-packages.el
+++ config/emacs-packages.el
@@ -1,7 +1,7 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa" . "http://melpa.org/packages/")))
 
 (package-initialize)
 (setq package-enable-at-startup nil)
