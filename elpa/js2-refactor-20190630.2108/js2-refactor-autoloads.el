4373ff2780faaefc390835b6e4ac35c38e1c7d47
Johan Lindstrom <johan.lindstrom@broadbean.com>
2020-04-07 17:02:42 +0000

    master: Upgrade js2-refactor

--- elpa/js2-refactor-20190630.2108/js2-refactor-autoloads.el
+++ elpa/js2-refactor-20190630.2108/js2-refactor-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "js2-refactor" "js2-refactor.el" (23376 54544
-;;;;;;  10423 350000))
+;;;### (autoloads nil "js2-refactor" "js2-refactor.el" (24204 45555
+;;;;;;  32433 762000))
 ;;; Generated autoloads from js2-refactor.el
 
 (autoload 'js2-refactor-mode "js2-refactor" "\
@@ -24,8 +24,8 @@ Add js2r keybindings using the modifier MODIFIER.
 
 ;;;***
 
-;;;### (autoloads nil "js2r-vars" "js2r-vars.el" (23376 54543 998423
-;;;;;;  500000))
+;;;### (autoloads nil "js2r-vars" "js2r-vars.el" (24204 45555 44433
+;;;;;;  611000))
 ;;; Generated autoloads from js2r-vars.el
 
 (autoload 'js2r-extract-var "js2r-vars" "\
@@ -48,7 +48,7 @@ Add js2r keybindings using the modifier MODIFIER.
 ;;;### (autoloads nil nil ("js2-refactor-pkg.el" "js2r-conditionals.el"
 ;;;;;;  "js2r-conveniences.el" "js2r-formatting.el" "js2r-functions.el"
 ;;;;;;  "js2r-helpers.el" "js2r-iife.el" "js2r-paredit.el" "js2r-wrapping.el")
-;;;;;;  (23376 54544 10423 350000))
+;;;;;;  (24204 45555 64433 360000))
 
 ;;;***
 
