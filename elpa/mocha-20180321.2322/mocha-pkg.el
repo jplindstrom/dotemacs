13ddc668e040ad0ed558dc2cf3b4206a91401c54
Johan Lindstrom <johan.lindstrom@broadbean.com>
2020-04-07 14:50:27 +0000

    master: mocha mode

---
+++ elpa/mocha-20180321.2322/mocha-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mocha" "20180321.2322" "Run Mocha or Jasmine tests" '((js2-mode "20150909") (f "0.18")) :commit "33e1b521a8a8d0225df353b51f1e8a4588ee32d0" :keywords '("javascript" "mocha" "jasmine") :authors '(("Al Scott")) :maintainer '("Al Scott") :url "http://github.com/scottaj/mocha.el")
