6b15f8963fbcd49002be88a9b148e4d42cd4d25d
Johan Lindstrom <johan.lindstrom@broadbean.com>
2020-05-24 16:35:09 +0000

    master: Upgrade treemacs, et al

---
+++ elpa/treemacs-icons-dired-20200510.2001/treemacs-icons-dired-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-icons-dired" "20200510.2001" "Treemacs icons for dired" '((treemacs "0.0") (emacs "25.2") (cl-lib "0.5")) :commit "1935108f683ac446e3d0add9467bc1adc4f744e7" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
---
+++ elpa/treemacs-projectile-20200114.1715/treemacs-projectile-autoloads.el
@@ -0,0 +1,24 @@
+;;; treemacs-projectile-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "treemacs-projectile" "treemacs-projectile.el"
+;;;;;;  (24266 40775 744379 395000))
+;;; Generated autoloads from treemacs-projectile.el
+
+(autoload 'treemacs-projectile "treemacs-projectile" "\
+Add one of `projectile-known-projects' to the treemacs workspace.
+With a prefix ARG was for the name of the project instead of using the name of
+the project's root directory.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; treemacs-projectile-autoloads.el ends here
