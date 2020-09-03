dccbbab842c1725ed698009e98b28944120003d0
Johan Lindstrom <johan.lindstrom@broadbean.com>
2020-09-03 10:59:27 +0100

    master: Upgrade poly-markdown, fixing the markdown-mode latex-mode issue \o/

--- elpa/poly-markdown-20200316.1315/poly-markdown-autoloads.el
+++ elpa/poly-markdown-20200316.1315/poly-markdown-autoloads.el
@@ -1,18 +1,15 @@
 ;;; poly-markdown-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "poly-markdown" "poly-markdown.el" (0 0 0 0))
+;;;### (autoloads nil "poly-markdown" "poly-markdown.el" (24400 48746
+;;;;;;  247886 851000))
 ;;; Generated autoloads from poly-markdown.el
  (autoload 'poly-markdown-mode "poly-markdown")
 
 (add-to-list 'auto-mode-alist '("\\.md\\'" . poly-markdown-mode))
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "poly-markdown" '("poly-markdown-")))
+ (autoload 'poly-gfm-mode "poly-markdown")
 
 ;;;***
 
@@ -20,6 +17,5 @@
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; poly-markdown-autoloads.el ends here
