036f7382362451a87779b1d3d3dc6309fe9de2ed
Johan Lindstrom <johan.lindstrom@broadbean.com>
2021-06-30 16:12:07 +0100

    master: lsp-mode

---
+++ elpa/lsp-mode-20210630.1249/lsp-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-mode" "20210630.1249" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "d87d0ccc05ef1da7fb46ea40d1d5ca7c9963357a" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
