e904d4322ee5f8d6cdb853b1102c3b89b5edb40c
Johan Lindstrom <johan.lindstrom@broadbean.com>
2021-11-28 12:48:36 +0000

    master: Upgrade JS packages

---
+++ elpa/js3-mode-20160515.1550/js3-mode-pkg.el
@@ -0,0 +1,9 @@
+(define-package "js3-mode" "20160515.1550" "An improved JavaScript editing mode" 'nil :commit "229aeb374f1b1f3ee5c59b8ba3eebb6385c232cb" :authors
+  '(("Thom Blake" . "webmaster@thomblake.com"))
+  :maintainer
+  '("Thom Blake" . "webmaster@thomblake.com")
+  :keywords
+  '("javascript" "languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
