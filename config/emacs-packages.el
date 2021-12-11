0e3d7ee645225941b6f3c78fa17c7e9301500c6c
Johan Lindstrom <johan.lindstrom@broadbean.com>
2021-12-11 18:21:42 +0000

    master: Remove marmalade repo

--- config/emacs-packages.el
+++ config/emacs-packages.el
@@ -1,6 +1,6 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ;; ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 
 (package-initialize)
