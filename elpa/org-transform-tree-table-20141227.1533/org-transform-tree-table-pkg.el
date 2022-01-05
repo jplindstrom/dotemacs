103ed4f9d298103bd1344321173a575c7835ba99
Johan Lindstrom <johan.lindstrom@broadbean.com>
2022-01-05 18:43:34 +0000

    master: Upgrade js2-mode

--- elpa/org-transform-tree-table-20141227.1533/org-transform-tree-table-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-transform-tree-table" "20141227.1533" "Transform org-mode tree with properties to a table, and the other way around" (quote ((dash "2.10.0") (s "1.3.0"))))
--- elpa/transient-20190812.1336/transient-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; transient-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("transient-pkg.el" "transient.el") (23893
-;;;;;;  21044 515155 352000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; transient-autoloads.el ends here
