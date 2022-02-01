8dfd601f119003cba86ee754470ff6eb5029f061
Johan Lindstrom <johan.lindstrom@broadbean.com>
2022-02-01 11:16:30 +0000

    master: Load all of tide for JS

--- config/lang-typescript.el
+++ config/lang-typescript.el
@@ -1,5 +1,36 @@
 (require 'flycheck)
 
+
+(defun setup-tide-keys ()
+  ;; company completion trigger
+  (local-set-key "\C-\M-SPC" 'company-tide)
+
+
+  ;; Go to
+  (local-set-key "\C-o\C-g" 'tide-jump-to-definition)
+  (local-set-key "\C-ogb" 'xref-pop-marker-stack)
+  ;; Docs
+  (local-set-key "\C-o\C-d" 'tide-documentation-at-point)
+  ;; Find
+  (local-set-key "\C-ofr" 'tide-references)
+
+  ;; Errors
+  (local-set-key "\C-ose" 'tide-project-errors)
+  (local-set-key "\C-oss" 'tide-error-at-point)
+
+  ;; Edit
+  (local-set-key "\C-oed" 'tide-jsdoc-template)
+
+  ;; Edit Refactor
+  ;; e C-r tide-refactor
+  (local-set-key "\C-o\C-d" 'tide-documentation-at-point)
+  (local-set-key "\C-oerr" 'tide-rename-symbol)
+  (local-set-key "\C-oerR" 'tide-rename-file)
+  (local-set-key "\C-oeri" 'tide-organize-imports)
+  (local-set-key "\C-oef" 'tide-fix)
+  (local-set-key "\C-oeF" 'tide-format)
+  )
+
 ;; https://github.com/ananthakumaran/tide
 (defun setup-tide-mode ()
   (interactive)
@@ -11,7 +42,10 @@
   ;; company is an optional dependency. You have to
   ;; install it separately via package-install
   ;; `M-x package-install [ret] company`
-  (company-mode +1))
+  (company-mode +1)
+
+  (setup-tide-keys))
+
 
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
@@ -20,49 +54,9 @@
 (add-hook 'before-save-hook 'tide-format-before-save)
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-
 (add-hook 'js2-mode-hook #'setup-tide-mode)
 ;; configure javascript-tide checker to run after your default javascript checker
 ;;; Doesn't work? (flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)
-
-
-
-;; Key bindings
-;; Tide
-(add-hook
- 'typescript-mode-hook
- '(lambda ()
-    (progn
-      ;; company completion trigger
-      (local-set-key "\C-\M-SPC" 'company-tide)
-
-
-      ;; Go to
-      (local-set-key "\C-o\C-g" 'tide-jump-to-definition)
-      (local-set-key "\C-ogb" 'xref-pop-marker-stack)
-      ;; Docs
-      (local-set-key "\C-o\C-d" 'tide-documentation-at-point)
-      ;; Find
-      (local-set-key "\C-ofr" 'tide-references)
-
-      ;; Errors
-      (local-set-key "\C-ose" 'tide-project-errors)
-      (local-set-key "\C-oss" 'tide-error-at-point)
-
-      ;; Edit
-      (local-set-key "\C-oed" 'tide-jsdoc-template)
-
-      ;; Edit Refactor
-      ;; e C-r tide-refactor
-      (local-set-key "\C-o\C-d" 'tide-documentation-at-point)
-      (local-set-key "\C-oerr" 'tide-rename-symbol)
-      (local-set-key "\C-oerR" 'tide-rename-file)
-      (local-set-key "\C-oeri" 'tide-organize-imports)
-      (local-set-key "\C-oef" 'tide-fix)
-      (local-set-key "\C-oeF" 'tide-format))
-    ))
-
 
 
 ;; Evil mode: don't use it in the tide buffers
