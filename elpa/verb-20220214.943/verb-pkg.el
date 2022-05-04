509f85db79bcbdc880fc4d131d0730d4b6cb32f9
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2022-05-04 11:13:46 +0100

    master: Install verb

---
+++ elpa/verb-20220214.943/verb-pkg.el
@@ -0,0 +1,12 @@
+(define-package "verb" "20220214.943" "Organize and send HTTP requests"
+  '((emacs "25.1"))
+  :commit "f6fd85d913c39603695e52d258d02e6030e3d42d" :authors
+  '(("Federico Tedin" . "federicotedin@gmail.com"))
+  :maintainer
+  '("Federico Tedin" . "federicotedin@gmail.com")
+  :keywords
+  '("tools")
+  :url "https://github.com/federicotdn/verb")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
