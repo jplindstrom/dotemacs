f048c779e1c9da73c987406fd91571351b9b34ea
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2022-07-18 12:00:07 +0100

    master: markdown-mode: use visual-line-mode

--- config/mode-markdown.el
+++ config/mode-markdown.el
@@ -9,3 +9,9 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . poly-markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . poly-markdown-mode))
 
+
+(defun jpl/set-visual-line-mode ()
+  (visual-line-mode 1))
+(add-hook 'markdown-mode-hook 'jpl/set-visual-line-mode)
+(add-hook 'gfm-mode-hook 'jpl/set-visual-line-mode)
+
