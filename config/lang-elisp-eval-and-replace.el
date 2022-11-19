a45b8ddc378b73cd822cec13050828072db66fb9
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2022-11-19 16:47:33 +0000

    master: elisp-mode: extremely hacky goto definition

--- config/lang-elisp-eval-and-replace.el
+++ config/lang-elisp-eval-and-replace.el
@@ -13,5 +13,14 @@
 
 
 
+;; Go to function definition
+(fset 'jpl/elisp-goto-defun
+   (kmacro-lambda-form [?\C-h ?f return C-tab tab return C-tab ?\C-x ?k return C-tab] 0 "%d"))
+
+(defun jpl/setup-elisp-mode ()
+  (interactive)
+  (local-set-key "\C-o\C-g" 'jpl/elisp-goto-defun))
+
+(add-hook 'emacs-lisp-mode-hook 'jpl/setup-elisp-mode)
 
 
