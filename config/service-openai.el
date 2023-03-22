0a6f8e0a215d324bff8506bbd97875b0dd2de99d
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2023-03-22 22:01:54 +0000

    master: Comment about org-ai

--- config/service-openai.el
+++ config/service-openai.el
@@ -5,3 +5,10 @@
 ;; https://github.com/karthink/gptel
 (setq gptel-api-key 'jpl/gptel-get-api-key)
 (global-set-key (kbd "C-o a a") 'gptel-send)
+
+
+;; https://github.com/rksm/org-ai
+;; Requires Emacs 28
+;; (setq org-ai-openai-api-token (jpl/gptel-get-api-key))
+
+
