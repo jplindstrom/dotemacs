169dbde380ea2e028cd572fef93bbe2731cb81c9
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2023-04-07 15:29:45 +0100

    master: Replace autopair with built in elecric-pair-mode

--- config/mode-autopair.el
+++ config/mode-autopair.el
@@ -1,7 +1,3 @@
 
-(require 'autopair)
+(electric-pair-mode)
 
-(add-hook 'graphviz-dot-mode-hook #'(lambda () (setq autopair-dont-activate t)))
-
-(autopair-global-mode) ;; to enable in all buffers
-(setq autopair-autowrap t)
