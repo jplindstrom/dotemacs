261571dbc426f9ce22c7fa62ecdf64932b69ddc3
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2023-05-12 22:49:24 +0100

    master: Markdown TAB to toggle heading overview

--- config/mode-markdown.el
+++ config/mode-markdown.el
@@ -10,9 +10,32 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . poly-markdown-mode))
 
 
+(defun jpl/markdown-outline-toggle (&optional arg)
+  "Visibility toggling for Markdown mode."
+  (interactive "P")
+
+  (cond
+   ;; Move from overview to all
+   ((eq markdown-cycle-global-status 2)
+    (outline-show-all)
+    (message "SHOW ALL")
+    (setq markdown-cycle-global-status 1))
+   ;; Defaults to overview
+   (t
+    (outline-hide-body)
+    (message "OVERVIEW")
+    (setq markdown-cycle-global-status 2)
+    (markdown-outline-fix-visibility))))
+
+
+
 (defun jpl/setup-markdown-mode ()
   (interactive)
   (visual-line-mode 1)
+
+  ;; Tab - toggle heading outline
+  (define-key evil-normal-state-local-map (kbd "C-i") 'jpl/markdown-outline-toggle)
+
 
   (define-key evil-normal-state-local-map (kbd "M-j") 'markdown-outline-next-same-level)
   (define-key evil-normal-state-local-map (kbd "M-k") 'markdown-outline-previous-same-level)
