2000288b6f9f455623e4ce74da0c0076443aabe1
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2023-06-01 17:04:06 +0100

    master: Remove treemacs

--- elpa/treemacs-evil-20230510.1644/treemacs-evil-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from treemacs-evil-20230510.1644/treemacs-evil.el  -*- no-byte-compile: t -*-
-(define-package "treemacs-evil" "20230510.1644" "Evil mode integration for treemacs" '((emacs "26.1") (evil "1.2.12") (treemacs "0.0")) :commit "12193dfa41d73f26386eacaeafd9ffaeee6491bd" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainers '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
