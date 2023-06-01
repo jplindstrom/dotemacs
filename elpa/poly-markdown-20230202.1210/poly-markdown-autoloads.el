550a33a628dd10b4ccd3b184d134c4450b474253
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2023-06-01 16:41:58 +0100

    master: upgrade poly-markdown

--- elpa/poly-markdown-20230202.1210/poly-markdown-autoloads.el
+++ elpa/poly-markdown-20230202.1210/poly-markdown-autoloads.el
@@ -1,15 +1,19 @@
 ;;; poly-markdown-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "poly-markdown" "poly-markdown.el" (24400 48746
-;;;;;;  247886 851000))
+;;;### (autoloads nil "poly-markdown" "poly-markdown.el" (0 0 0 0))
 ;;; Generated autoloads from poly-markdown.el
  (autoload 'poly-markdown-mode "poly-markdown")
 
 (add-to-list 'auto-mode-alist '("\\.md\\'" . poly-markdown-mode))
  (autoload 'poly-gfm-mode "poly-markdown")
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "poly-markdown" '("poly-")))
 
 ;;;***
 
@@ -17,5 +21,6 @@
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; poly-markdown-autoloads.el ends here
