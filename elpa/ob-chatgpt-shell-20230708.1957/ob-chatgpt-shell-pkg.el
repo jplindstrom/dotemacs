165d726f186342b1c42ac0dff72b079fd5671ef8
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2023-08-05 12:31:44 +0100

    master: chatgpt-shell, ob-chatgpt-shell for org-mode source blocks

---
+++ elpa/ob-chatgpt-shell-20230708.1957/ob-chatgpt-shell-pkg.el
@@ -0,0 +1,13 @@
+(define-package "ob-chatgpt-shell" "20230708.1957" "Org babel functions for ChatGPT evaluation"
+  '((emacs "27.1")
+    (chatgpt-shell "0.60.1"))
+  :commit "345f9d0e9047091474a2cea3d12b6e52b804ae36" :authors
+  '(("Alvaro Ramirez"))
+  :maintainers
+  '(("Alvaro Ramirez"))
+  :maintainer
+  '("Alvaro Ramirez")
+  :url "https://github.com/xenodium/chatgpt-shell")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
