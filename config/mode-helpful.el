cd68d59034994357d9cb621be3eb2a16e8021e0b
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2023-08-12 15:37:16 +0100

    master: helpful

---
+++ config/mode-helpful.el
@@ -0,0 +1,8 @@
+
+(require 'helpful)
+
+(global-set-key (kbd "C-h f") #'helpful-callable)
+
+(global-set-key (kbd "C-h v") #'helpful-variable)
+(global-set-key (kbd "C-h k") #'helpful-key)
+(global-set-key (kbd "C-h x") #'helpful-command)
