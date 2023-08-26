4c1f6ef4de130f76b98a69fa6195a098d93ac5c1
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2023-08-26 16:49:35 +0100

    master: Add model name

--- config/service-openai.el
+++ config/service-openai.el
@@ -24,14 +24,13 @@
   "Return message with a vivid color and bold."
   (message (propertize message 'face '(:foreground "red" :weight "bold"))))
 
-(defun jpl/llm-run-template (template)
-  "Run the specific 'llm' TEMPLATE on current selection or entire buffer.
-Specify the llm variable 'major_mode'"
-  (interactive "sTemplate name: ")
+(defun jpl/llm-run-template (template model)
+  "Run the specific 'llm' TEMPLATE and MODEL on current selection or entire buffer."
+  (interactive "sTemplate name: \nsModel: ")
   (let* ((start (if (use-region-p) (region-beginning) (point-min)))
          (end (if (use-region-p) (region-end) (point-max)))
          (programming-language (replace-regexp-in-string "-mode$" "" (symbol-name major-mode)))
-         (command (concat "llm -t " template " -p programming_language " programming-language))
+         (command (concat "llm -t " template " -m " model " -p programming_language " programming-language))
          (original-point (point)))
     (message (format "%s (%s)" (jpl/propertize-busy "Running llm...") command))
     (shell-command-on-region start end command nil t)
@@ -40,8 +39,7 @@ Specify the llm variable 'major_mode'"
 (defun jpl/llm-fix ()
   "Run 'llm' using the 'fix' template on current selection or entire buffer."
   (interactive)
-  (jpl/llm-run-template "fix"))
-
+  (jpl/llm-run-template "fix" "4"))
 
 (global-set-key (kbd "C-o a f") 'jpl/llm-fix)
 
