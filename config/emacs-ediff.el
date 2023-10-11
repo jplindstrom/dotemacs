ce593c2d3e800a24511a5ee0718fa70ceee999da
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2023-10-11 10:27:01 +0100

    master: ediff config

--- config/emacs-ediff.el
+++ config/emacs-ediff.el
@@ -1,4 +1,8 @@
 
 (setq ediff-split-window-function 'split-window-horizontally)
 
+;; Don't show the control buffer in its own frame (which ALWAYS ends
+;; up out of focus)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
 
