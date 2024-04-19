0077338185171a7b18ad968a0c016be5194d351e
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-04-19 16:26:31 +0100

    master: jpl/increase-number-at-point

--- config/lang-text-utils.el
+++ config/lang-text-utils.el
@@ -3,8 +3,10 @@
 (defun jpl/increase-number-at-point (&optional increase)
   "Increase the number at point with the number that is the prefix argument"
   (interactive "P")
-  (let ((num (or (thing-at-point 'number) 0)))
-    (replace-match (number-to-string (+ num (prefix-numeric-value increase))))))
+  (save-excursion
+    (let ((num (or (thing-at-point 'number) 0)))
+      (replace-match (number-to-string (+ num (prefix-numeric-value increase)))))))
 
 
-(global-set-key (kbd "\C-o e n") 'jpl/increase-number-at-point)
+(global-set-key (kbd "\C-o e n i") 'jpl/increase-number-at-point)
+
