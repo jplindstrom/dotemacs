fb03d81f937be0678dc89ae6c160f5f0365960a3
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-08-13 17:54:45 +0100

    master: Disable package signature checking, to avoid
    Failed to verify signature archive-contents.sig:
    No public key for 645357D2883A0966 created at 2024-08-13T10:05:03+0100 using EDDSA
    Command output:
    gpg: Signature made Tue 13 Aug 2024 10:05:03 BST
    gpg:                using EDDSA key 0327BE68D64D9A1A66859F15645357D2883A0966
    gpg: Can't check signature: No public key

--- config/emacs-packages.el
+++ config/emacs-packages.el
@@ -3,6 +3,7 @@
                          ;; ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 
+(setq package-check-signature nil) ;; YOLO
 ;;;JPL: remove (package-initialize)
 (setq package-enable-at-startup nil)
 
