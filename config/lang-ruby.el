d49769b5b1e452d1bdb82721d2b896d86e7b0a1c
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-08-13 18:36:26 +0100

    master: remove inf-ruby, not used

--- config/lang-ruby.el
+++ config/lang-ruby.el
@@ -9,14 +9,4 @@
                                      interpreter-mode-alist))
 
 
-(autoload 'run-ruby "inf-ruby"
-  "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby"
-  "Set local key defs for inf-ruby in ruby-mode")
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-             (inf-ruby-keys)
-             ))
-
-
 
