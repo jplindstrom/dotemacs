db67274296e46b02a11405d25856ab06ecfb41fa
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-08-13 19:07:55 +0100

    master: Replace ruby-mode.el with builtin

--- config/lang-ruby.el
+++ config/lang-ruby.el
@@ -1,8 +1,6 @@
 
 ;; Ruby
 
-(autoload 'ruby-mode "ruby-mode"
-  "Mode for editing ruby source files" t)
 (setq auto-mode-alist
       (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
 (setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
