dbba26cc4f3f1e31a1264aa90302ec385e7e674f
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-08-13 17:55:08 +0100

    master: Set elpa.gnu.org to use https

--- config/emacs-packages.el
+++ config/emacs-packages.el
@@ -1,5 +1,5 @@
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ;; ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 
