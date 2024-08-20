064e0dacebccc54e6c94b70dfec775c89faa2b23
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-08-20 19:19:49 +0100

    master: Typo

--- config/emacs-clipboard.el
+++ config/emacs-clipboard.el
@@ -1,6 +1,6 @@
 
 ;;JPL: check if it works without this
-;; Integrate the X11 clipboard witht the Gnome window manager
+;; Integrate the X11 clipboard with the Gnome window manager
 (setq x-select-enable-clipboard t)
 (if (functionp 'x-cut-buffer-or-selection-value)
     (setq interprogram-paste-function 'x-cut-buffer-or-selection-value))
