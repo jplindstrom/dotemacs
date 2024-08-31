64dc20f9943fd8285ed7193b8dc4c7c65c197a4d
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-08-31 15:20:30 +0100

    master: Upgrade transient

---
+++ elpa/transient-20240830.1817/transient-pkg.el
@@ -0,0 +1,16 @@
+(define-package "transient" "20240830.1817" "Transient commands"
+  '((emacs "26.1")
+    (compat "30.0.0.0")
+    (seq "2.24"))
+  :commit "dd8c2999b1e3ca93e6fd7d171e38da2cc57eea90" :authors
+  '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers
+  '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainer
+  '("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")
+  :keywords
+  '("extensions")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
