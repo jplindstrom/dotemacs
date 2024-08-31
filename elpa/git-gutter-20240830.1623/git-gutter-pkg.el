0cc48412b87ffafc550045ea05607f79e650de9e
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-08-31 19:11:54 +0100

    master: Upgrade git-gutter

---
+++ elpa/git-gutter-20240830.1623/git-gutter-pkg.el
@@ -0,0 +1,13 @@
+(define-package "git-gutter" "20240830.1623" "Port of Sublime Text plugin GitGutter"
+  '((emacs "25.1"))
+  :commit "47e443809f352b5188751175413ebe0f1eb2311f" :authors
+  '(("Syohei YOSHIDA" . "syohex@gmail.com"))
+  :maintainers
+  '(("Neil Okamoto" . "neil.okamoto+melpa@gmail.com")
+    ("Jen-Chieh" . "jcs090218@gmail.com"))
+  :maintainer
+  '("Neil Okamoto" . "neil.okamoto+melpa@gmail.com")
+  :url "https://github.com/emacsorphanage/git-gutter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
