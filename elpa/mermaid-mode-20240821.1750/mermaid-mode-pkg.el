e2be8b6b9d819dd90513f8877c97b2735e25cf4b
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-09-16 13:51:52 +0100

    master: mermaid-mode

---
+++ elpa/mermaid-mode-20240821.1750/mermaid-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "mermaid-mode" "20240821.1750" "Major mode for working with mermaid graphs"
+  '((emacs "25.3"))
+  :commit "fc8ae220e8d7769114072493975df821b68df6b3" :keywords
+  '("mermaid" "graphs" "tools" "processes")
+  :url "https://github.com/abrochard/mermaid-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
