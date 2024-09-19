83be5992d542d75abc5dcc5e0d110c038e3f7019
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-09-19 23:44:11 +0100

    master: disable helpful, way too slow

--- config/mode-helpful.el
+++ config/mode-helpful.el
@@ -1,8 +1,8 @@
 
-(require 'helpful)
+;; (require 'helpful)
 
-(global-set-key (kbd "C-h f") #'helpful-callable)
+;; (global-set-key (kbd "C-h f") #'helpful-callable)
 
-(global-set-key (kbd "C-h v") #'helpful-variable)
-(global-set-key (kbd "C-h k") #'helpful-key)
-(global-set-key (kbd "C-h x") #'helpful-command)
+;; (global-set-key (kbd "C-h v") #'helpful-variable)
+;; (global-set-key (kbd "C-h k") #'helpful-key)
+;; (global-set-key (kbd "C-h x") #'helpful-command)
