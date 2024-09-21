2edc081cc6b20a8a42cf9b7b2cf73116e1100967
Johan Lindstrom <johan.lindstrom@zoopla.co.uk>
2024-09-21 11:38:14 +0100

    master: Unset C-o prefix key early in the config

--- config/emacs-global-unset-keys.el
+++ config/emacs-global-unset-keys.el
@@ -1,7 +1,8 @@
 
-(global-unset-key "\C-t")
+(global-unset-key "\C-t")        ;; Useless M-x transpose-chars
 
-(global-unset-key (kbd "C-;"))
+(global-unset-key "\C-o")        ;; Used by my PerlySense key prefix
+(global-unset-key (kbd "C-;"))   ;; Used by my personal text expansions
 
 ;; Fuck this page!
 (global-unset-key (kbd "C-h h"))
