8995f8c150d024f854529f5706e4833825dcb42f
Johan <johan.lindstrom@zoopla.co.uk>
2024-10-12 13:15:41 +0100

    master: Mac key config

--- config/emacs-mac.el
+++ config/emacs-mac.el
@@ -1,10 +1,8 @@
 
 
-(setq ns-command-modifier 'meta)
-
 (when (eq system-type 'darwin) ;; mac specific settings
-  (setq mac-option-modifier 'super)
-  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'meta)
+  (setq mac-command-modifier 'super)
   )
 
 
