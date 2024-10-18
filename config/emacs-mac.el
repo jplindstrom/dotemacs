28bb6b58329d2d76b81d121f45ebfa8e61c4d1c5
Johan <johan.lindstrom@zoopla.co.uk>
2024-10-18 14:29:24 +0100

    master: Unbind macOS Cmd key clashes

--- config/emacs-mac.el
+++ config/emacs-mac.el
@@ -3,6 +3,13 @@
 (when (eq system-type 'darwin) ;; mac specific settings
   (setq mac-option-modifier 'meta)
   (setq mac-command-modifier 'super)
+
+  ;; Unbind common mistypes of the Cmd button
+  (global-set-key (kbd "s-x") nil)
+  (global-set-key (kbd "s-h") nil)
+  (global-set-key (kbd "s-j") nil)
+  (global-set-key (kbd "s-k") nil)
+  (global-set-key (kbd "s-l") nil)
   )
 
 
